7fa8759939d99043b1b7b3eaced804595484e99f
oleksandr-suslov <76252100+oleksandr-suslov@users.noreply.github.com>
2021-06-03 11:39:53 +0300

    изменил согласно правок

--- js/task-05.js
+++ js/task-05.js
@@ -9,5 +9,8 @@ const span = document.querySelector('#name-output');
     input.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
-    span.textContent = event.currentTarget.value;
+    if (event.currentTarget.value == '') {
+        span.textContent ='незнакомец'
+    }
+    else ( span.textContent = event.currentTarget.value );
 };
